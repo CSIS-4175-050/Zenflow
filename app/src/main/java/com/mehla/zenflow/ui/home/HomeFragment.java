c74e79033e0b533ffb48653ee498713f5cd6757c
Gaurav Mehla <gaurav@mehla.in>
2023-07-20 19:39:10 -0700

    Popular image on homepage

--- app/src/main/java/com/mehla/zenflow/ui/home/HomeFragment.java
+++ app/src/main/java/com/mehla/zenflow/ui/home/HomeFragment.java
@@ -1,6 +1,9 @@
 package com.mehla.zenflow.ui.home;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -9,8 +12,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -20,12 +26,25 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.mehla.zenflow.R;
 import com.mehla.zenflow.databinding.FragmentHomeBinding;
+import com.mehla.zenflow.interfaces.ExerciseApi;
+import com.mehla.zenflow.model.Exercise;
+import com.squareup.picasso.Picasso;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class HomeFragment extends Fragment {
@@ -37,6 +56,18 @@ public class HomeFragment extends Fragment {
     Button btnRefreshQuote;
 
     static ArrayList<String> quotes;
+
+
+
+
+    private TextView txtPopularWorkoutTitle, cardTitle, cardDesc;
+    private ImageView imageView;
+    private ProgressBar imageLoading;
+    private SharedPreferences sharedPreferences;
+    private List<Exercise> exerciseList;
+
+    private static final int MAX_RETRIES = 3;
+    private int retryCount = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -62,7 +93,78 @@ public class HomeFragment extends Fragment {
         });
         refreshQuote();
 
+        txtPopularWorkoutTitle = root.findViewById(R.id.txt_popular_workout_title);
+        cardTitle = root.findViewById(R.id.card_title);
+        cardDesc = root.findViewById(R.id.card_desc);
+        imageView = root.findViewById(R.id.imageView);
+        imageLoading = root.findViewById(R.id.image_loading);
+
+        sharedPreferences = getActivity().getSharedPreferences("app", Context.MODE_PRIVATE);
+
+        fetchExercises();
+
         return root;
+    }
+
+    private void fetchExercises() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://raw.githubusercontent.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ExerciseApi exerciseApi = retrofit.create(ExerciseApi.class);
+        Call<List<Exercise>> call = exerciseApi.getExercises();
+
+        call.enqueue(new retrofit2.Callback<List<Exercise>>() {
+            @Override
+            public void onResponse(Call<List<Exercise>> call, Response<List<Exercise>> response) {
+                if (!response.isSuccessful()) {
+                    if (isAdded()) {
+                        Toast.makeText(getContext(), "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    }
+                    return;
+                }
+
+                exerciseList = response.body();
+                updatePopularWorkout();
+            }
+
+            @Override
+            public void onFailure(Call<List<Exercise>> call, Throwable t) {
+                Toast.makeText(getContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updatePopularWorkout() {
+        if(retryCount < MAX_RETRIES) {
+            Random random = new Random();
+            int index = random.nextInt(exerciseList.size());
+            Exercise exercise = exerciseList.get(index);
+
+            cardTitle.setText(exercise.getSanskritName());
+            cardDesc.setText(exercise.getDescription());
+
+            Log.d(">>", exercise.getSanskritName());
+
+            imageLoading.setVisibility(View.VISIBLE);
+            Picasso.get()
+                    .load(exercise.getImage())
+                    .into(imageView, new com.squareup.picasso.Callback() {
+                        @Override
+                        public void onSuccess() {
+                            imageLoading.setVisibility(View.GONE);
+                            retryCount = 0;
+                        }
+
+                        @Override
+                        public void onError(Exception e) {
+                            imageLoading.setVisibility(View.GONE);
+                            retryCount++;
+                            updatePopularWorkout();
+                        }
+                    });
+        }
     }
 
 
