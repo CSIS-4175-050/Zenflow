9a0ffa0cc8c69ab559c447ac313c9fb20ecf28b8
Gaurav Mehla <gaurav@mehla.in>
2023-08-15 16:56:42 -0700

    commited using shortcut key

--- app/src/main/java/com/mehla/zenflow/ui/workouts/WorkoutAdapter.java
+++ app/src/main/java/com/mehla/zenflow/ui/workouts/WorkoutAdapter.java
@@ -9,9 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.Timestamp;
 import com.mehla.zenflow.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHolder> {
@@ -24,12 +28,18 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHold
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         CardView cardView;
-        TextView workoutTitle;  // Adjust as needed
+        TextView workoutTitle;
+        TextView workoutTime;
+        TextView workoutDate;
+        TextView workoutCreationTime;
 
         public ViewHolder(View v) {
             super(v);
-            cardView = v.findViewById(R.id.cardView);  // Adjust the ID as needed
-            workoutTitle = v.findViewById(R.id.workout_title);  // Adjust as needed
+            cardView = v.findViewById(R.id.cardView);
+            workoutTitle = v.findViewById(R.id.workout_title);
+            workoutTime = v.findViewById(R.id.workout_time);
+            workoutDate = v.findViewById(R.id.workout_date);
+            workoutCreationTime = v.findViewById(R.id.creation_datetime);
         }
     }
 
@@ -43,8 +53,36 @@ public class WorkoutAdapter extends RecyclerView.Adapter<WorkoutAdapter.ViewHold
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Map<String, Object> workout = workoutsData.get(position);
-        // Set data to the views here. Example:
-        holder.workoutTitle.setText(workout.get("ExerciseName").toString());
+
+        String exerciseName = (String) workout.get("ExerciseName");
+        holder.workoutTitle.setText(exerciseName);
+
+        Long yearLong = (Long) workout.get("Year");
+        int year = yearLong.intValue();
+
+        Long monthLong = (Long) workout.get("Month");
+        int month = monthLong.intValue();
+
+        Long dayLong = (Long) workout.get("Day");
+        int day = dayLong.intValue();
+
+        String dateFormatted = String.format(Locale.getDefault(), "%d-%02d-%02d", year, month, day);
+        holder.workoutDate.setText("Date: " + dateFormatted);
+
+        Long minuteLong = (Long) workout.get("Minute");
+        int minute = minuteLong.intValue();
+
+        Long secondLong = (Long) workout.get("Second");
+        int second = secondLong.intValue();
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minute, second);
+        holder.workoutTime.setText("Time: " + timeFormatted);
+
+        // Assuming creationDate is a timestamp. If not, further parsing might be required.
+        Timestamp creationTimestamp = (Timestamp) workout.get("creationDate");
+        Date creationDate = creationTimestamp.toDate();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        String creationDateTime = sdf.format(creationDate);
+        holder.workoutCreationTime.setText(creationDateTime);
     }
 
     @Override
